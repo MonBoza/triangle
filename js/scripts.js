bdafa352fc1808e073a5aa0c8de2b2793cdade36
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-10-19 18:30:24 -0500

    updated js code Co-Authored-by:Hunter Empey<LizardMak@gmail.com> CO-Authored-by:Aaron Mejia<aaron@mejia.dev>

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,33 @@
-function hideResults(){
-    document.getElementById("Not").setAttribute("class", "hidden");
+function hideResults() {
+    document.getElementById("not").setAttribute("class", "hidden");
     document.getElementById("Equilateral").setAttribute("class", "hidden");
     document.getElementById("Isosceles").setAttribute("class", "hidden");
     document.getElementById("Scalene").setAttribute("class", "hidden");
 }
 
 
-function calculateTri (event) {
-    document.querySelector("form").onsubmit = function (event) {
-     hideResults();
-     event.preventDefault();
-     const side1 = parseInt(document.querySelector("input#side1")).value;
-     const side2 = parseInt(document.querySelector("input#side2")).value;
-     const side3 = parseInt(document.querySelector("input#side3")).value;
-     if (side1 && side2 && side3) {
-        if (side1 === side2&&side2 === side3);
-            document.getElementById.apply("Equilateral").removeAttribute("class");
-        } else if (side1 === side2||side2=== side3);{
-            document.getElementById("Isosceles").removeAttribute("class")
-        } else if (side1 !== side2 && side2 !== side3)
-            document.getElementById.apply("Scalene").removeAttribute.("class");
+function calculateTri(event) {
+    hideResults();
+    event.preventDefault();
+    const side1 = parseInt(document.querySelector("input#side1").value);
+    const side2 = parseInt(document.querySelector("input#side2").value);
+    const side3 = parseInt(document.querySelector("input#side3").value);
+    if ((side1 + side2 <= side3) || (side1 + side3 <= side2) || (side3 + side2 <= side1)) {
+        document.getElementById("not").removeAttribute("class");
+    } else if (side1 === side2 && side2 === side3) {
+        document.getElementById("Equilateral").removeAttribute("class");
+    } else if (side1 === side2 || side1 === side3 || side2 === side3) {
+        document.getElementById("Isosceles").removeAttribute("class")
+    } else if ((side1 != side2) && (side2 != side3)) {
+        document.getElementById("Scalene").removeAttribute("class");
+    } else {
+        document.getElementById("not").removeAttribute("class");
     }
-    
-    }
+
+}
 
 
-
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
     const form = document.getElementById("triCalc");
     form.addEventListener("submit", calculateTri);
 });
-
-
